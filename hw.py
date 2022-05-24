dbf6d7abf0510310005d371200881d00fd41605c
juliaKukhareva <105663188+juliaKukhareva@users.noreply.github.com>
2022-05-24 22:21:19 +0200

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,10 @@
+name = "Julia"
+color = "grey"
+film = "Pirates of the Caribbean"
+music_groups = "depend on my mood"
+print ("Hello!")
+print ("My name is " + " " + name)
+print ("A few facts about myself.")
+print ("My favorite color is" + " " + color)
+print ( film + " is my favorite film.")
+print ("Music" + " " + music_groups)
