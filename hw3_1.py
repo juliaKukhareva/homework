467a9f4d355f4b0f57bb3e8f7ed6460219ffa612
juliaKukhareva <105663188+juliaKukhareva@users.noreply.github.com>
2022-06-12 22:20:45 +0200

    Add files via upload

---
+++ hw3_1.py
@@ -0,0 +1,57 @@
+# calculator
+
+
+q1 = int (input ('Введите число 1: '))
+q2 = int (input ('Введите число 2: '))
+
+v = int (input ('Какую операцию вы хотите выполнить? /n 1 Сложение/ n 2 Вычитание/n 3 Деление/ n 4 Умножение/ n'))
+
+if v == 1:
+    r = q1+q2
+    p = 'сложения'
+    t=p
+if v == 2:
+    r = q1-q2
+    l = 'вычитания'
+    t=l
+if v == 3:
+    r = float (q1/q2)
+    m = 'деления'
+    t=m
+if v == 4:
+    r = q1*q2
+    n = 'умножения'
+    t=n
+print ('Результат ', t, '=', r)
+
+# turtle 
+import turtle
+
+
+square = turtle.Turtle()
+square.shape("turtle")
+turtle.color ("Red")
+for i in range(4):
+    turtle.right(90)
+    turtle.forward(100)
+    
+turtle.penup ()
+turtle.forward (100)
+turtle.pendown ()
+
+
+rectangle = turtle.Turtle()
+rectangle.shape("turtle")
+turtle.color ("yellow")
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+
+turtle.exitonclick()
+
+
